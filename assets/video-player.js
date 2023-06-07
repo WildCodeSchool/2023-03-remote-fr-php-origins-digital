09fb7ae1d015a16c5784d0048d1d52b6798d492a
vravanel <vravanel@gmail.com>
2023-06-07 10:11:33 +0200

    player video ok

--- assets/video-player.js
+++ assets/video-player.js
@@ -1,11 +1,49 @@
-import videojs from "video.js";
-import './styles/_video.scss';
+import videojs from "video.js"
+import "video.js/dist/video-js.css"
+import './styles/_video.scss'
+import 'video.js/dist/video'
+import 'videojs-playlist/dist/videojs-playlist'
 
-var player = videojs('myVideo', {
-    autoplay: 'muted',
-    controls: true,
-    loop: true,
-    fluid: true,
-    aspectRatio: '4:3',
-    playbackRates: [0.25,0.5,1,1.5,2.5,3]
-})
+const player = videojs('my-player', {
+  autoplay: 'muted',
+  controls: 'true',
+  loop: false,
+  aspectRatio: '4:3',
+  playbackRates: [0.25,0.5,1,1.5,2,2.5],
+});
+
+player.playlist([{
+  sources: [{
+    src: 'http://media.w3.org/2010/05/sintel/trailer.mp4',
+    type: 'video/mp4'
+  }],
+  poster: 'http://media.w3.org/2010/05/sintel/poster.png'
+}, {
+  sources: [{
+    src: 'http://media.w3.org/2010/05/bunny/trailer.mp4',
+    type: 'video/mp4'
+  }],
+  poster: 'http://media.w3.org/2010/05/bunny/poster.png'
+}, {
+  sources: [{
+    src: 'http://vjs.zencdn.net/v/oceans.mp4',
+    type: 'video/mp4'
+  }],
+  poster: 'http://www.videojs.com/img/poster.jpg'
+}, {
+  sources: [{
+    src: 'http://media.w3.org/2010/05/bunny/movie.mp4',
+    type: 'video/mp4'
+  }],
+  poster: 'http://media.w3.org/2010/05/bunny/poster.png'
+}, {
+  sources: [{
+    src: 'http://media.w3.org/2010/05/video/movie_300.mp4',
+    type: 'video/mp4'
+  }],
+  poster: 'http://media.w3.org/2010/05/video/poster.png'
+}]);
+
+player.playlist.autoadvance(0);
+
+
