ee30236e9a439ab182e0a1c694917a3b927470b4
BrianV87 <Brian.v87@hotmail.fr>
2023-06-09 22:51:54 +0200

    Création page/interface admin

---
+++ src/Controller/VideoAdminController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\VideoRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/video/admin', name: 'video_admin_')]
+class VideoAdminController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(VideoRepository $videoRepository): Response
+    {
+        $videos = $videoRepository->findBy([], ['id' => 'asc']);
+        return $this->render('admin/video_admin/index.html.twig', [
+            'videos' => $videos,
+        ]);
+    }
+}
