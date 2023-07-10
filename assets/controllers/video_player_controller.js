2c1ab97b4d1da2d938723ba361a8a9bcc3b9a184
BrianV87 <Brian.v87@hotmail.fr>
2023-07-10 11:26:27 +0200

    Modal fini a 90%, manque lien colonne de droite vidéoÃ

--- assets/controllers/video_player_controller.js
+++ assets/controllers/video_player_controller.js
@@ -6,12 +6,12 @@ import '../styles/_video.scss'
 export default class extends Controller {
     connect()
     {
-      window.player = videojs(this.element, {
-        controls: true,
-        loop: false,
-        playbackRates: [0.25, 0.5, 1, 1.5, 2, 2.5],
-        autoload: true,
-      });
+        window.player = videojs(this.element, {
+            controls: true,
+            loop: false,
+            playbackRates: [0.25, 0.5, 1, 1.5, 2, 2.5],
+            autoload: true,
+        });
     }
 }
 
