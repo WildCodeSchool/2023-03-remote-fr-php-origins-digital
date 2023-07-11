bbade149299bc8ad07c1ca328856e818a97beb83
vravanel <vravanel@gmail.com>
2023-07-11 09:23:13 +0200

    ajout du tour js

---
+++ assets/intro.js
@@ -0,0 +1,26 @@
+import 'intro.js/introjs.css';
+
+import introJs from 'intro.js';
+
+introJs().setOptions({
+    steps: [{
+        intro: "Bonjour. Bienvenue dans votre espace administrateur !"
+    },
+    {
+        element: document.querySelector(".step-user"),
+        intro: "Ici vous pouvez voir, ajouter, modifier, supprimer vos utilisateurs.",
+    },
+    {
+        element: document.querySelector(".step-category"),
+        intro: "Ici vous pouvez voir, ajouter, modifier, supprimer vos categories.",
+    },
+    {
+        element: document.querySelector(".step-tags"),
+        intro: "Ici vous pouvez voir, ajouter, modifier, supprimer vos tags.",
+    },
+    {
+        element: document.querySelector(".step-videos"),
+        intro: "Ici vous pouvez voir, ajouter, modifier, supprimer vos vidéos.",
+    },
+    ],
+}).start();
